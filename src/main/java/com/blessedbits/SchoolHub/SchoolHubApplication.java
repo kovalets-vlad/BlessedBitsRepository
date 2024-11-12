888efb4d4ac0fa9aa0bfb40aa4ce3894f3f1ef37
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2024-11-12 20:04:23 +0200

    Add starting backend

---
+++ src/main/java/com/blessedbits/SchoolHub/SchoolHubApplication.java
@@ -0,0 +1,13 @@
+package com.blessedbits.SchoolHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blessedbits/SchoolHub/SchoolHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessedbits.SchoolHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
